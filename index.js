b29c63c8810587e1cda26a238cdf2f8e1da4e1fb
triciahughes <hughes.a.tricia@gmail.com>
2023-01-20 14:34:25 -0800

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+document.getElementById("main").remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+document.body.appendChild(newHeader);
+
+newHeader.textContent = "You is the champion.";
